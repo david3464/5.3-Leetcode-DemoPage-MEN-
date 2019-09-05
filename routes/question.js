a9fa175e6ddc7d5eb974dbd59a3ac9d288657985
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-05 18:11:10 -0400

    set all category directory correctly

--- routes/question.js
+++ routes/question.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 // app.use('/question', questionRouter);
 
+
+//router address localhost:4121/question/new
+//descriptions: Register New Question
+//comments: show register demo form
+router.get('/', function(req, res, next) {
+    res.render('question/question_list');
+});
+
 //router address localhost:4121/question/new
 //descriptions: Register New Question
 //comments: show register demo form
