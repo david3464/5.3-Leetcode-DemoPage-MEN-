be62eca58974b57cbcb413cb91d35f63c1e4f8ba
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-06 12:48:00 -0400

    1

--- models/Question.js
+++ models/Question.js
@@ -2,30 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //Create Question Schema
 const QuestionSchema = new Schema({
-    title : {
-            type: String,
-            required: true
-    },
-    Description : {
-            type: String,
-            required: true
-    },
-    type : {
-            type: String,
-            required: true
-    },
-    analyze : {
-            type: String,
-            required: true
-    },
-    tip : {
-            type: String,
-            required: true
-    },
-    date : {
-            type: Date,
-            default:Date.now
-    }
+        title : {
+                type: String,
+                required: true
+        },
+        number : {
+                type: Number,
+                required: true
+        },
+        type : {
+                type: mongoose.Schema.type.ObjectID,
+                required: true,
+                ref: 'Category'
+        },
+        Description : {
+                type: String
+        },
+        analyze : {
+                type: String
+        },
+        solution : {
+                type: String
+        },
+        tip : {
+                type: String
+        },
+        create_date : {
+                type: Date,
+                required:true,
+                default:Date.now
+        },
+        coverImageName: {
+                type: String,
+                required: true,
+        },
+
 });
 const Question = mongoose.model('Question', QuestionSchema);
 module.exports = Question;
