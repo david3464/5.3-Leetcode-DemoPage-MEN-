1d556f990a570a5175f2efbf7d76a93b14ca80a8
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-09 16:05:58 -0400

    link database of both category and question

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+Question = require('./Question');
 //Create Category Schema
 const CategorySchema = new Schema({
     category_name : {
@@ -15,5 +16,19 @@ const CategorySchema = new Schema({
         default:Date.now
     }
 });
+
+CategorySchema.pre('remove', function(next){
+    Question.find ({type:this.id}, (err, questions)=> {
+       if(err){
+           next(err)
+       } else if (questions.length>0){
+           next(new Error (`This category has ${questions.length} questions in database stills`))
+       } else {
+           next()
+       }
+    })
+})
+
+
 const Category = mongoose.model('Category', CategorySchema);
 module.exports = Category;
--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 Category = require('../models/Category');
+Question = require('../models/Question');
 // app.use('/category', categoryRouter);
 
 //router address localhost:4121/category
@@ -50,7 +51,7 @@ router.post('/new', async (req, res, next)=> {
         res.redirect('/category')
   } 
   catch(err) {
-        console.log('err during during 4121/category/new create new category '+err);
+        console.log('err during during post 4121/category/new create new category '+err);
         res.render('category/register_type', {
         category: category,
         error: 'Error in Creating Author'
@@ -61,8 +62,20 @@ router.post('/new', async (req, res, next)=> {
 //router address localhost:4121/category/:id
 //descriptions: Show Detail Question Information
 //comments: Show detail information of a Question
-router.get('/:id', (req, res, next)=> {
-  res.send('Show Category' + req.params.id)
+router.get('/:id', async (req, res, next)=> {
+    try {
+      const category = await Category.findById(req.params.id)
+      // console.log(category.category_name)
+      const questions = await Question.find({type : category.id})
+      // console.log(questions)
+      res.render('category/show_type_question', {
+                category : category,
+                questions: questions
+      })
+    } catch (err) {
+      console.log(err)
+      res.redirect('/')
+    }
 });
 
 //router address localhost:4121/category/:id/edit
@@ -73,9 +86,8 @@ router.get('/:id/edit', async (req, res, next)=> {
     let category = await Category.findById(req.params.id)
     res.render('category/edit_type', { category: category });
   } catch(err) {
-    console.log('err during during 4121/category '+err);
+    console.log('err during during 4121/category:id/edit '+err);
     res.redirect('/')
-
   }
 
 });
@@ -83,15 +95,47 @@ router.get('/:id/edit', async (req, res, next)=> {
 //router address localhost:4121/question/:id (Behind the Scene)
 //descriptions: Update Detail Category-Type Information
 //comments: Change detail information of a Type
-router.put('/:id', (req, res, next)=> {
-  res.send('Update Category' + req.params.id)
+router.put('/:id/edit', async (req, res, next)=> {
+  let category 
+  try { 
+      category = await Category.findById(req.params.id)
+                 category.type_name = req.body.type_name
+                 await category.save()
+      res.redirect(`/category/${category.id}`)
+  } 
+  catch(err) {
+      if (author == null) {
+         console.log('err during during put 4121/category/:id can not find category-type on exist database'+err);
+         res.redirect('/')
+      } else {
+        console.log('err during during 4121/category/:id update category-type '+err);
+        res.redirect('/')
+      }
+    }
 });
 
 //router address localhost:4121/question/:id (Behind the Scene)
 //descriptions: Delete Specific Category-Type
 //comments: Detail this type from Category database
-router.delete('/:id', (req, res, next)=> {
-  res.send('Delete Category' + req.params.id)
+router.delete('/:id', async (req, res, next)=> {
+  let category 
+  try {
+      let deletecategoryid = req.params.id
+      category = await Category.findById(deletecategoryid)
+      console.log(category)
+      console.log(category.id)
+                 await category.remove()
+      res.redirect('/category')
+  } 
+  catch(err) {
+      if (category == null) {
+         console.log('err during during delete 4121/category/:id can not find category-type on exist database'+err);
+         res.redirect('/')
+      } else {
+        console.log('err during during 4121/category/:id delete category-type '+err);
+        res.redirect('/category')
+      }
+    }
 });
 
 //router address localhost:4121/category/:category_name/:type_name
