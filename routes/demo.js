62f2f94866de2e9e9bf50611dcd5f240d7bffe7a
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-20 14:43:42 -0400

    question 07

--- routes/demo.js
+++ routes/demo.js
@@ -9,7 +9,7 @@ router.get('/1', async (req, res, next)=>{
   let question = await Question.find({number:'1'});
   // console.log(question);
   console.log(question[0].title);
-  res.render('demo/07Reverse_Integer',{question : question});
+  res.render('demo/01Two_Sum',{question : question});
   } catch(err) {
     console.log('err during during 4121/demo/01 (Two Sum) '+err);
   }
@@ -23,7 +23,7 @@ router.get('/7', async (req, res, next)=>{
   let question = await Question.find({number:'7'});
   // console.log(question);
   console.log(question[0].title);
-  res.render('demo/01Two_Sum',{question : question});
+  res.render('demo/07Reverse_Integer',{question : question});
   } catch(err) {
     console.log('err during during 4121/demo/07 (Reverse Integer) '+err);
   }
